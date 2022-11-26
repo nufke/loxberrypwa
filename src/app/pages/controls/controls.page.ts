41b56fb22c1df8ad20da4d7d18ffb5aa4a80c9c4
nufke <nufke@barnasconi.net>
2022-11-27 00:35:18 +0100

    filter controls

--- src/app/pages/controls/controls.page.ts
+++ src/app/pages/controls/controls.page.ts
@@ -21,6 +21,7 @@ export class ControlsPage
   public items: any[];
   public labels: any[];
 
+  public filtered_controls: Control[];
   private filtered_categories: string[];
   private filtered_rooms: string[];
 
@@ -55,17 +56,14 @@ export class ControlsPage
     this.controlsSub = LoxBerryService.getControls().subscribe((controls: Control[]) => {
       this.controls = controls
       .sort( (a, b) => { return a.order - b.order || a.name.localeCompare(b.name) })
-      .filter( item => item.is_visible === true);
 
       this.filtered_categories = controls
         .map(item => item.category )
-        .filter((value, index, self) => self.indexOf(value) === index) // remove duplicates
+        //.filter((value, index, self) => self.indexOf(value) === index) // remove duplicates
 
       this.filtered_rooms = controls
         .map(item => item.room )
-        .filter((value, index, self) => self.indexOf(value) === index) // remove duplicates
-
-      this.updateControls(controls);
+        //.filter((value, index, self) => self.indexOf(value) === index) // remove duplicates
     });
 
     this.categoriesSub = LoxBerryService.getCategories().subscribe((categories: Category[]) => {
@@ -75,6 +73,7 @@ export class ControlsPage
 
       if (this.domain === 'category') {
         this.page = this.findObj(categories, this.hwid, this.uuid);
+        this.filtered_controls = this.controls.filter( item => (item.category === this.page.uuid && item.is_visible ));
         this.items = categories;
       }
 
@@ -89,6 +88,7 @@ export class ControlsPage
 
       if (this.domain === 'room') {
         this.page = this.findObj(rooms, this.hwid, this.uuid);
+        this.filtered_controls = this.controls.filter( item => (item.room === this.page.uuid && item.is_visible ));
         this.items = rooms;
       }
 
@@ -112,29 +112,16 @@ export class ControlsPage
     }
   }
 
-  private findName(obj: any, uuid: string): Control {
-    return obj.find( item => { return (item.uuid == uuid) } );
-  }
-
   private findObj(obj: any, hwid: string, uuid: string): Control {
-    return obj.find( item => { return (item.uuid == uuid) && (item.hwid == hwid) } );
+    return obj.find( item => (item.uuid == uuid) && (item.hwid == hwid) );
   }
 
-  public filter(item: any, label: any) : Control[] {
-    return item.filter( resp => { return (resp[this.key] === label.uuid) &&
-      (this.findName(this.items, resp[this.domain]).name === this.page.name)
-    });
+  public filter(label: any) : Control[] {
+    return this.filtered_controls.filter( resp => resp[this.key] === label.uuid );
   }
 
-  public is_empty(item: any, label: any) : Boolean {
-    return (this.filter(item, label).length > 0);
-  }
-
-  private updateControls(controls: Control[])
-  {
-    controls.forEach( item => {
-      this.updateDisplay(item)
-    });
+  public is_empty(label: any) : Boolean {
+    return (this.filter(label).length > 0);
   }
 
 }
