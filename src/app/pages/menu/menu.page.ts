75aa064116537fa3baec4e96e45469ef497fdc5b
nufke <nufke@barnasconi.net>
2023-01-03 23:58:02 +0100

    refactoring control updates, store and services,

--- src/app/pages/menu/menu.page.ts
+++ src/app/pages/menu/menu.page.ts
@@ -61,7 +61,7 @@ export class MenuPage implements OnInit {
 
   onToggleDarkTheme() {
     document.body.classList.toggle('dark', this.darkTheme);
-    console.log("darkTheme: ", this.darkTheme);
+    //console.log("darkTheme: ", this.darkTheme);
     this.storageService.store( { appDarkTheme: this.darkTheme } );
   }
 
